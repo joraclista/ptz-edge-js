4efb694be38a9925864dfd24b47932c464f9c032
Alisa Popova <alisa.popova@gmail.com>
2021-04-05 14:14:59 +0300

    added base module structure

---
+++ index.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+
+var edge = require('electron-edge-js');
+
+//process.env.EDGE_USE_CORECLR = 1;
+
+
+
+const assemblyFileDll = 'PTZDevice.dll';
+const baseDllPath = assemblyFileDll;
+
+
+const ptzClassName = 'PTZ.PTZDeviceHelper';
+
+var getCameraZoomInfo = edge.func({
+    assemblyFile: baseDllPath,
+    typeName: ptzClassName,
+    methodName: 'GetCameraZoomInfo'
+});
+
+var zoomInCamera = edge.func({
+    assemblyFile: baseDllPath,
+    typeName: ptzClassName,
+    methodName: 'ZoomIn'
+});
+
+var zoomOutCamera = edge.func({
+    assemblyFile: baseDllPath,
+    typeName: ptzClassName,
+    methodName: 'ZoomOut'
+});
+
+var moveCamera = edge.func({
+    assemblyFile: baseDllPath,
+    typeName: ptzClassName,
+    methodName: 'Move'
+});
+
+module.exports = {
+   getCameraZoomInfo,
+   zoomInCamera,
+   zoomOutCamera,
+   moveCamera
+}
+
---
+++ renderer.js
@@ -0,0 +1,82 @@
+const path = require('path');
+
+
+var edge = require('electron-edge-js');
+var ptz = require('./index.js');
+
+process.env.EDGE_USE_CORECLR = 1;
+
+console.log('### Calling methods from C# code');
+
+
+window.onload = function() {
+	
+	var getIntFromId = function(id) {
+		return parseInt(document.getElementById(id).value);
+	}
+
+    var updateZoomInfo = () => ptz.getCameraZoomInfo('PTZ Pro 2', function(error, result) {
+        if (error) throw error;
+        document.getElementById("GetCameraZoomInfo").innerHTML = JSON.stringify(result);
+    });
+	
+	updateZoomInfo();
+
+    document.getElementById("ZoomIn").onclick = function() {
+		ptz.zoomInCamera({'name':'PTZ Pro 2'}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+			updateZoomInfo();
+		});
+	}
+
+	document.getElementById("ZoomOut").onclick = function() {
+		ptz.zoomOutCamera({'name':'PTZ Pro 2'}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+			updateZoomInfo();
+		});
+	}
+	
+	document.getElementById("Pan").onclick = function() {
+		ptz.moveCamera({'name':'PTZ Pro 2', 'pan':getIntFromId('PanValue'), 'tilt':0}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+		});
+	}
+	
+	document.getElementById("Left").onclick = function() {
+		ptz.moveCamera({'name':'PTZ Pro 2', 'pan':-500, 'tilt':0}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+		});
+	}
+	
+	document.getElementById("Right").onclick = function() {
+		ptz.moveCamera({'name':'PTZ Pro 2', 'pan':500, 'tilt':0}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+		});
+	}
+	
+	document.getElementById("Tilt").onclick = function() {
+		ptz.moveCamera({'name':'PTZ Pro 2', 'pan':0, 'tilt':getIntFromId('TiltValue')}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+		});
+	}
+	document.getElementById("Up").onclick = function() {
+		ptz.moveCamera({'name':'PTZ Pro 2', 'pan':0, 'tilt':500}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+		});
+	}
+	document.getElementById("Down").onclick = function() {
+		ptz.moveCamera({'name':'PTZ Pro 2', 'pan':0, 'tilt':-500}, function (error, result) {
+			if (error) throw error;
+			console.log(result);
+		});
+	}
+	
+
+}
